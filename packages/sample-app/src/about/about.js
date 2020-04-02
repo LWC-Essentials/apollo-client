6f4ec9bf0929e864bdb3138f3cb4339171b87409
Philippe Riand <priand@salesforce.com>
2020-04-02 18:24:37 -0400

    first try of router

--- packages/sample-app/src/about/about.js
+++ packages/sample-app/src/about/about.js
@@ -4,8 +4,8 @@
     SPDX-License-Identifier: BSD-3-Clause
     For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
 */
-import { LightningElement } from 'lwc';
+import { LightningElement, } from './node_modules/lwc';
 
-export default class Header extends LightningElement {
+export default class App extends LightningElement {
 
 }
